cd8d5c3332b51487a2f20c4d9360fac25994a32e
dellam <delabena29@gmail.com>
2019-08-07 18:03:42 +0000

    changed settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'jh4(b1zli(cx_wut&5fv08obe=1=^-7csje=#bba-dd6&c&#^%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
 
 
 # Application definition
